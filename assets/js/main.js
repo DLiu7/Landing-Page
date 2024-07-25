f297f2e41f9b7e53502f2e0440925a30c2c5177a
DauntlessZX <134324641+DauntlessZX@users.noreply.github.com>
2024-07-25 18:03:14 -0400

    Update to Information
    Filling out forms and customizing certain UI design choices.

--- assets/js/main.js
+++ assets/js/main.js
@@ -188,3 +188,25 @@
 			});
 
 })(jQuery);
+
+function contact(event) {
+    event.preventDefault();
+    const loading = document.querySelector(".modal__overlay--loading");
+    const success = document.querySelector(".modal__overlay--success");
+    loading.classList += " modal__overlay--visible"
+    emailjs
+        .sendForm(
+            "service_5pfyxcu",
+            "template_0l39ewb",
+            event.target,
+            "OGVq5jbZr4x4aaLdm"
+        ).then(() => {
+            loading.classList.remove("modal__overlay--visible");
+            success.classList += " modal__overlay--visible";
+        }).catch(() => {
+            loading.classList.remove("modal__overlay--visible");
+            alert(
+                "The email service is temporarily unavailable. Please contact me directly on kzhang0826@gmail.com"
+            );
+        })
+}
